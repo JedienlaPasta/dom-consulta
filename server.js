d87525234cc133edbdbdaaae429cca67e479cba9
Seba <sebastian1377@gmail.com>
2022-05-11 11:15:58 -0400

    second setup attempt

--- server.js
+++ server.js
@@ -16,22 +16,22 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
 
-app.use('/roles', rolesRoutes)
-app.use('/permisos', permisosRoutes)
-app.use('/users', userRoutes)
-
-// app.get('/', (req, res) => {
-//     res.send('Welcome to Roles API')
-// })
-
 const PORT = process.env.PORT || 5000
 
 const CONNECTION_URL = 'mongodb+srv://mongo:BELC6ZxeB53MnQza@rolescluster.u7c3e.mongodb.net/rolesDB?retryWrites=true&w=majority'
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
+
+app.use('/roles', rolesRoutes)
+app.use('/permisos', permisosRoutes)
+app.use('/users', userRoutes)
 
 mongoose.connect(process.env.CONNECTION_URL || CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
+// app.get('/', (req, res) => {
+//     res.send('Welcome to Roles API')
+// })
