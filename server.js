f557366483df785b5657af1e38294b838db5c168
Seba <sebastian1377@gmail.com>
2022-05-11 11:17:53 -0400

    third setup attempt

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 const CONNECTION_URL = 'mongodb+srv://mongo:BELC6ZxeB53MnQza@rolescluster.u7c3e.mongodb.net/rolesDB?retryWrites=true&w=majority'
 
